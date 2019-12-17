5ef41605ab5312391367d7b29a160b7bc800bd03
IamPopov <ivantsar@gmail.com>
2019-12-16 18:35:30 -0700

    created basic structure and readme file

---
+++ quiz.js
@@ -0,0 +1,49 @@
+// Inside which HTML element do we put the JavaScript?
+
+// <script>  
+//   Your answer  
+// <js>
+// <scripting>
+// <javascript>
+
+// Question 2:
+// What is the correct JavaScript syntax to change the content of the HTML element below?  <p id="demo">This is a demonstration.</p>
+
+// document.getElementById("demo").innerHTML = "Hello World!";  
+//   Your answer  
+// #demo.innerHTML = "Hello World!";
+// document.getElement("p").innerHTML = "Hello World!";
+// document.getElementByName("p").innerHTML = "Hello World!";
+
+// Question 3:
+// Where is the correct place to insert a JavaScript?
+
+// The <body> section  
+//   Your answer  
+// The <head> section
+// Both the <head> section and the <body> section are correct  
+//   Correct answer  
+
+// Question 4:
+// What is the correct syntax for referring to an external script called "xxx.js"?
+
+// <script src="xxx.js">  
+//   Your answer  
+// <script href="xxx.js">
+// <script name="xxx.js">
+
+// Question 5:
+// The external JavaScript file must contain the <script> tag.
+
+// False  
+//   Your answer  
+// True
+
+// Question 6:
+// How do you write "Hello World" in an alert box?
+
+// alert("Hello World");  
+//   Your answer  
+// msgBox("Hello World");
+// msg("Hello World");
+// alertBox("Hello World");
